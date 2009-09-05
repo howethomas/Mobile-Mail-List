2d93c53fe29da1e7bee393ec1f68479a3b6835ac
Thomas <howethomas@aol.com>
2009-09-04 23:07:43 -0400

    Added basic menu, formatting, login... stuff

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -17,4 +17,43 @@ module ApplicationHelper
     messages
   end
   
+  def logged_in?
+       session[:email] && session[:password]
+     end
+
+    # Is this from a Polycom phone?
+    def polycom_user_agent?
+      request.env["HTTP_USER_AGENT"] && request.env["HTTP_USER_AGENT"][/PolycomSoundPointIP/]
+    end
+
+
+
+     def logout
+       link_to "Logout", :controller => "sessions", :action => "destroy"
+     end
+     def dashboard
+       link_to "Dashboard", :controller => "dashboard", :action => "index" 
+     end
+
+     def format_phone_number(phone_number)
+       phone_number = phone_number.to_s
+       match = phone_number.match /^1(\d{3})(\d{3})(\d{4})$/
+       return phone_number unless match
+       npa, nxx, xxxx = match.captures
+       "1 (#{npa}) #{nxx}-#{xxxx}"
+     end
+
+     def humanize_hour(hour)
+       if hour == 0
+         "12am"
+       elsif hour < 12
+         "#{hour}am"
+       elsif hour == 12
+         "12pm"
+       else
+         "#{hour - 12}pm"
+       end
+     end
+  
+  
 end
