ff4ae2b0165ab8510968b02ab72d5a6d37e1070d
Thomas <howethomas@aol.com>
2009-09-10 18:07:56 -0400

    End of the day on thursday - lots of stuff working

--- app/controllers/subscribers_controller.rb
+++ app/controllers/subscribers_controller.rb
@@ -40,8 +40,13 @@ class SubscribersController < ApplicationController
   # POST /subscribers
   # POST /subscribers.xml
   def create
+    puts params.inspect
+#    puts request.session.inspect
+#    puts request.parameters.inspect
+    puts request.env.inspect
+    
     @subscriber = Subscriber.new(params[:subscriber])
-
+    
     respond_to do |format|
       if @subscriber.save
         flash[:notice] = 'Subscriber was successfully created.'
